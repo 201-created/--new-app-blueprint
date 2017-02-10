4b90240eca4126a55cfa1a68289894d4b7cd19cd
Isaac Ezer <isaac.ezer@gmail.com>
2017-02-10 11:28:32 -0500

    Add component in legacy path to test fallback

--- src/resolver.js
+++ src/resolver.js
@@ -1,3 +1,6 @@
 import Resolver from 'dangerously-set-unified-resolver';
 
+// Use this to skip fallback. Will not render {{legacy-component}}
+// import Resolver from 'dangerously-set-unified-resolver/turbo';
+
 export default Resolver.extend();
