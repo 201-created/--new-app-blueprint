74b11eeb375916e9146ec595775617d69e66fa57
Isaac Ezer <isaac.ezer@gmail.com>
2017-02-10 11:47:23 -0500

    add pods component

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'my-app',
+    podModulePrefix: 'my-app/pods',
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
